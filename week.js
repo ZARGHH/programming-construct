676977e788aa821e73ec1b92f86a76ca38c0e9b5
zargham <zarghamahmedkhan@gmailcom>
2020-12-07 02:00:54 +0530

    Added: days of week added using case statement

--- week.js
+++ week.js
@@ -1,28 +1,29 @@
-const prompt = require( 'prompt= sync') ();
-const number = prompt("enter number");
-switch(Number(number)) {
-case 1:
-console.log("sunday");
-break;
-case 2:
-console.log("monday");
-break;
-case 3:
-console.log("tuesday");
-break;
-case 4:
-console.log("wednesday");
-break;
-case 5:
-console.log("thursday");
-break;
-case 6:
-console.log("friday");
-break;
-case 7:
-console.log("saturday");
-break;
-default:
-console.log("enter the number between 1 to 7");
-break;
+function checkWeek(number) {
+  switch (number) {
+    case 0:
+      console.log("Sunday");
+      break;
+    case 1:
+      console.log("Monday");
+      break;
+    case 2:
+      console.log("Tuesday");
+      break;
+    case 3:
+      console.log("Wednesday");
+      break;
+    case 4:
+      console.log("Thursday");
+      break;
+    case 5:
+      console.log("Friday");
+      break;
+    case 6:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid Number");
+  }
 }
+
+console.log(checkWeek(4));
