1dccd6c9a4eee5119ee800d8086a28a6703073a5
zargham <zarghamahmedkhan@gmailcom>
2020-12-07 00:17:43 +0530

    the maximum and minimum of the number

---
+++ employee.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Employee Wage Computation Program");
+//const Employee = require('./utility');
+let Utility=require('./utility');
+let Tech = new Utility.Employee("Tech Mahindra", 20, 10, 10);
+Tech.calculateWage();
+//const Google = Utility.Employee("Google",20,20,160);
+
---
+++ maxmin.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Assignment of If Else If");
+let number1 = Math.floor(Math.random()*999) ;
+let number2 = Math.floor(Math.random()*999) ;
+let number3 = Math.floor(Math.random()*999) ;
+let number4 = Math.floor(Math.random()*999) ;
+let number5 = Math.floor(Math.random()*999) ;
+
+console.log(number1+","+number2+","+number3+","+number4+","+number5);
+
+console.log(Math.max(number1,number2,number3,number4,number5));
+console.log(Math.min(number1,number2,number3,number4,number5));
