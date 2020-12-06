575e13232120bad65dccafec5c29f57533803b56
zargham <zarghamahmedkhan@gmailcom>
2020-12-07 01:45:07 +0530

    the place of the digit added using switch case

--- week.js
+++ week.js
@@ -1,28 +1,28 @@
 const prompt = require( 'prompt= sync') ();
 const number = prompt("enter number");
 switch(Number(number)) {
-case 1;
+case 1:
 console.log("sunday");
 break;
-case 2;
+case 2:
 console.log("monday");
 break;
-case 3;
+case 3:
 console.log("tuesday");
 break;
-case 4;
+case 4:
 console.log("wednesday");
 break;
-case 5;
+case 5:
 console.log("thursday");
 break;
-case 6;
+case 6:
 console.log("friday");
 break;
-case 7;
+case 7:
 console.log("saturday");
 break;
-default;
+default:
 console.log("enter the number between 1 to 7");
 break;
 }
